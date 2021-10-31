04d685acb5dd54589001b247abd417a5ff5ecf40
tlorgeree <71152544+tlorgeree@users.noreply.github.com>
2021-10-31 18:04:09 -0400

    Finished course

--- JS- courses/script.js
+++ JS- courses/script.js
@@ -22,3 +22,28 @@ else if (direction == "backward")
 else console.log("You crashed and died a violent space death");
 
 
+//Function declaration
+function hourToMinutes(hours){
+    return hours*60;
+}
+
+console.log(hourToMinutes(50));
+
+
+//Working with DOM
+//1. Select
+
+let sign = document.getElementById("sign");
+console.log(sign);
+
+//2. Modify
+console.log(sign.textContent);
+sign.textContent = "Go away!";
+
+sign.innerHTML = sign.innerHTML + "<p>hello</p>";// add html without changing existing
+
+//3. Change style
+sign.style.color = "green";
+
+
+
