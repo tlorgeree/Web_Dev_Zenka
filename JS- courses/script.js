10baef99f8cf12b45a336aed940fbf3cabd5eb15
tlorgeree <71152544+tlorgeree@users.noreply.github.com>
2021-10-31 14:22:19 -0400

    Conditional statements

--- JS- courses/script.js
+++ JS- courses/script.js
@@ -1,1 +1,24 @@
 console.log("this works");
+let b = 1;
+b++;
+
+console.log("b is: " + b);//Console log allows multiple data types
+
+//Conditional statements
+let direction = "lefst";
+
+if (direction == "left")
+{
+    console.log("Red Planet");
+}
+else if (direction == "right")
+{
+    console.log("Purple Planet")
+}
+else if (direction == "backward")
+{
+    console.log("Going home");
+}
+else console.log("You crashed and died a violent space death");
+
+
